ebd7c74bfe91a4e546b2877d18a2c34ccc498b0b
Bethelhem2 <bettywoldeamanue@gmail.com>
2024-04-12 00:35:33 +0300

    project started

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoEZQ8-_9-fBjzs8ehdWMwTuf-TRF-xjc",
+    authDomain: "netflix-clone-96777.firebaseapp.com",
+    projectId: "netflix-clone-96777",
+    storageBucket: "netflix-clone-96777.appspot.com",
+    messagingSenderId: "723627899320",
+    appId: "1:723627899320:web:25a523e25cc2e3bc6e374d"
+  };
